53d450c4acd1f3472352169ac12ebfcb2caa141d
sushilashrestha <susshhh23@gmail.com>
2024-12-04 22:04:09 +0545

    Refactor TeamCard component for improved layout and styling

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,31 +1,28 @@
-import { Team } from '@/types';
+import { Team } from '@/types'
 
 interface TeamCardProps {
-  team: Team;
+  team: Team
 }
 
 export default function TeamCard({ team }: TeamCardProps) {
   return (
-    <a
+    <a 
       href={`/teams/2024/${team.id}`}
-      className="group block bg-white rounded-lg shadow-sm overflow-hidden transition-all hover:shadow-md w-64 mx-4" // Set width and margin between cards
+      className="group block bg-white rounded-lg shadow-sm overflow-hidden transition-all hover:ring-2 hover:ring-secondary hover:shadow-lg hover:shadow-cyan-400/20 p-2 mx-2 my-3"
     >
-      <div className="relative w-full mx-2">
-        {/* Center the image and apply padding to the right */}
-        <div className="flex justify-center pt-4 pr-4"> {/* pt-4: padding-top, pr-4: padding-right */}
-          <img
-            src={`${team.imageUrl}`}
-            alt={`${team.name}`}
-            className="object-cover h-auto w-full transition-transform group-hover:scale-105"
-          />
-        </div>
+      <div className="relative h-36 w-36 mx-auto mb-4">
+        <img
+          src={`${team.imageUrl}`}
+          alt={`${team.name}`}
+          className="object-cover h-full w-full rounded-lg transition-transform group-hover:scale-110"
+        />
       </div>
-      <div className="p-4"> {/* Padding for content */}
+      <div className="p-3">
         <h2 className="text-base font-semibold text-gray-800 group-hover:text-gray-600 mb-1">
           {team.name}
         </h2>
         <p className="text-xs text-gray-600">{team.institution}</p>
       </div>
     </a>
-  );
+  )
 }
